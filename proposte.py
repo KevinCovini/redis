9810b684fc5e04ec2d9aae05737b7eefedcc1449
kcovini <kcovini@itsrizzoli.it>
2023-06-26 09:20:21 +0200

    Aggiunti commenti per funzioni presenti Unificato linguaggio in italiano

--- proposte.py
+++ proposte.py
@@ -3,13 +3,13 @@ import re
 import time
 import pandas as pd
 
-
+# Connessione al DB Redis
 r = rd.Redis(
     host='redis-12900.c300.eu-central-1-1.ec2.cloud.redislabs.com',
     port=12900,
     password='uUlKJLiadqj12nYHVKcwK9TowwyuXrvb')
 
-''' Templates
+''' Esempio Templates
 
 macchinette = {
     titolo: "macchinette",
@@ -30,13 +30,16 @@ macchinette_votanti = set(
 
 '''
 
-
 def insertProposal(p_number=1):
+    '''Funzione di inserimento proposta richedendo queste info:
+    Proponenti (chi fa la proposta), titolo e descrizione.
+    La proposta verrà scritta in un hash e i proponenti in un set separato.
+    '''
     for n in range(p_number):
         proposers = re.sub(r"[^\w\s]", "", input(
-            "Insert name(s) of the proposer(s): ")).split()
-        proposal_title = input("Insert the title of the proposal: ")
-        proposal_desc = input("Insert the description of the proposal: ")
+            "Inserisci cognome/i dei proponenti: ")).split()
+        proposal_title = input("Inserisci il nome della proposta: ")
+        proposal_desc = input("Inserisci la descrizione della proposta: ")
         r.hset(f"{proposal_title} proposta", mapping={
             "Titolo": proposal_title,
             "Descrizione": proposal_desc
@@ -46,6 +49,7 @@ def insertProposal(p_number=1):
 
 
 def showProposal():
+    '''Stampa tutte le proposte inserite in DB'''
     for i, key in enumerate(r.keys("*proposta")):
         print(f"{i+1}. {r.hgetall(key)[b'Titolo'].decode()} (", end="")
         for prop in r.smembers(f"{key.decode()[:-8]}proponenti"):
@@ -57,16 +61,14 @@ def showProposal():
             print("0 voti\n")
 
 def voteProposal():
-    # inserisci cognome (login)
-    # inserisci id proposta
-    # se hai già votato la proposta vai a fare in culo
-    # altrimenti: cognome -> db (proposta.lista votanti), voti+=1
-
-    cognome = input("Insert last name: ")
-    proposal_id = input("Insert proposal ID: ")
-
+    '''Richiede Cognome e ID proposta per inserire la votazione di essa:
+    se la proposta è già stata votata non viene effettuata nessuna operazione sul DB;
+    Se la proposta non è già stata votata verrà inserita la votazione per la proposta corrispondente.
+    '''
+    cognome = input("Inserisci il tuo cognome: ")
+    proposal_id = input("Inserisci il titolo della proposta da votare: ")
     if r.sismember(f"voters:{proposal_id}", cognome):
-        print("You already voted this proposal.")
+        print("Hai già votato per questa proposta")
     else:
         r.sadd(f"voters:{proposal_id}", cognome)
         r.hincrby(f"proposal:{proposal_id}", "votes")
@@ -74,4 +76,5 @@ def voteProposal():
 
 if __name__ == "__main__":
     # insertProposal()
-    showProposal()
+    # showProposal()
+    print(r.keys())
